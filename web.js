74ad487605d7d07d32c9bf01b45d4b94c0dc9369
Ubuntu <ubuntu@ip-172-31-16-70.us-west-2.compute.internal>
2013-07-11 16:12:25 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,10 +4,10 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-response.send('aaa');
+
 fs.readFile('index.html', function (err, data) {
   if (err) throw err;
- response.send(data);
+ response.send(data.toString());
 });
 });
 
